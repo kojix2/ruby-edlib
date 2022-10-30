f08ba4d8f6e1107af19fc3ffb2cc044c3ec07cbd
kojix2 <2xijok@gmail.com>
2022-10-30 19:13:28 +0900

    Accept kwargs

--- lib/edlib.rb
+++ lib/edlib.rb
@@ -2,5 +2,12 @@ require_relative 'edlib/edlib'
 
 module Edlib
   class Aligner
+    def initialize(query, target, k:1, mode: 'NW', task: 'DISTANCE', additional_equalities: nil)
+      mode = mode.to_s if mode.is_a? Symbol
+      task = task.to_s if task.is_a? Symbol
+      mode = mode.upcase if mode.is_a? String
+      task = task.upcase if task.is_a? String
+      initialize_raw(query, target, k, mode, task, additional_equalities)
+    end
   end
 end

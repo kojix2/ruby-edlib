62a3a602a695dd3e6fe83b96062eb3b4369f096e
kojix2 <2xijok@gmail.com>
2022-10-30 22:07:46 +0900

    Fix default value k

--- lib/edlib.rb
+++ lib/edlib.rb
@@ -2,7 +2,7 @@ require_relative 'edlib/edlib'
 
 module Edlib
   class Aligner
-    def initialize(k:1, mode: 'NW', task: 'DISTANCE', additional_equalities: nil)
+    def initialize(k:-1, mode: 'NW', task: 'DISTANCE', additional_equalities: nil)
       mode = mode.to_s if mode.is_a? Symbol
       task = task.to_s if task.is_a? Symbol
       mode = mode.upcase if mode.is_a? String
---
+++ test/edlib_test.rb
@@ -0,0 +1,26 @@
+require_relative "test_helper"
+
+class EdlibTest < Test::Unit::TestCase
+  def test_that_it_has_a_version_number
+    refute_nil ::Edlib::VERSION
+  end
+
+  def test_initilize
+    assert_instance_of Edlib::Aligner, Edlib::Aligner.new
+  end
+
+  def test_config
+    assert_instance_of Hash, Edlib::Aligner.new.config
+  end
+
+  def test_align
+    assert_instance_of Hash, Edlib::Aligner.new.align("A", "A")
+  end
+
+  def test_k
+    a = Edlib::Aligner.new
+    assert_equal -1, a.k
+    a.k = 2
+    assert_equal 2, a.k
+  end
+end

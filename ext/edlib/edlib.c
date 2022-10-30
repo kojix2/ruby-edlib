7e529c847deec8d59b6ed4989d4ac1957a638915
kojix2 <2xijok@gmail.com>
2022-10-30 16:56:34 +0900

    Add methods to set config parameters

--- ext/edlib/edlib.c
+++ ext/edlib/edlib.c
@@ -65,6 +65,20 @@ aligner_get_k(VALUE self)
 }
 
 static VALUE
+set_k(EdlibAlignConfig *config, VALUE k)
+{
+	config->k = NUM2INT(k);
+	return k;
+}
+
+static VALUE
+aligner_set_k(VALUE self, VALUE k)
+{
+	EdlibAlignConfig *config = get_config(self);
+	return set_k(config, k);
+}
+
+static VALUE
 get_mode(EdlibAlignConfig *config)
 {
 	switch (config->mode)
@@ -85,6 +99,68 @@ aligner_get_mode(VALUE self)
 {
 	EdlibAlignConfig *config = get_config(self);
 	return get_mode(config);
+}
+
+static VALUE
+set_mode(EdlibAlignConfig *config, VALUE mode)
+{
+	switch (TYPE(mode))
+	{
+	case T_STRING:
+		if (strcmp(RSTRING_PTR(mode), "HW") == 0)
+		{
+			config->mode = 0;
+		}
+		else if (strcmp(RSTRING_PTR(mode), "SHW") == 0)
+		{
+			config->mode = 1;
+		}
+		else if (strcmp(RSTRING_PTR(mode), "NW") == 0)
+		{
+			config->mode = 2;
+		}
+		else
+		{
+			rb_raise(rb_eArgError, "Invalid mode");
+		}
+		break;
+	case T_SYMBOL:
+		if (SYM2ID(mode) == rb_intern("HW"))
+		{
+			config->mode = 0;
+		}
+		else if (SYM2ID(mode) == rb_intern("SHW"))
+		{
+			config->mode = 1;
+		}
+		else if (SYM2ID(mode) == rb_intern("NW"))
+		{
+			config->mode = 2;
+		}
+		else
+		{
+			rb_raise(rb_eArgError, "Invalid mode");
+		}
+		break;
+	case T_FIXNUM:
+		int m = NUM2INT(mode);
+		if (m < 0 || m > 2)
+		{
+			rb_raise(rb_eArgError, "Invalid mode");
+		}
+		config->mode = NUM2INT(mode);
+		break;
+	default:
+		rb_raise(rb_eArgError, "Invalid mode");
+	}
+	return mode;
+}
+
+static VALUE
+aligner_set_mode(VALUE self, VALUE mode)
+{
+	EdlibAlignConfig *config = get_config(self);
+	return set_mode(config, mode);
 }
 
 static VALUE
@@ -111,6 +187,61 @@ aligner_get_task(VALUE self)
 }
 
 static VALUE
+set_track(EdlibAlignConfig *config, VALUE task)
+{
+	switch (TYPE(task))
+	{
+	case T_STRING:
+		if (strcmp(RSTRING_PTR(task), "DISTANCE") == 0)
+		{
+			config->task = 0;
+		}
+		else if (strcmp(RSTRING_PTR(task), "LOC") == 0)
+		{
+			config->task = 1;
+		}
+		else if (strcmp(RSTRING_PTR(task), "PATH") == 0)
+		{
+			config->task = 2;
+		}
+		else
+		{
+			rb_raise(rb_eArgError, "Invalid task");
+		}
+		break;
+	case T_SYMBOL:
+		if (SYM2ID(task) == rb_intern("DISTANCE"))
+		{
+			config->task = 0;
+		}
+		else if (SYM2ID(task) == rb_intern("LOC"))
+		{
+			config->task = 1;
+		}
+		else if (SYM2ID(task) == rb_intern("PATH"))
+		{
+			config->task = 2;
+		}
+		else
+		{
+			rb_raise(rb_eArgError, "Invalid task");
+		}
+		break;
+	case T_FIXNUM:
+		int t = NUM2INT(task);
+		if (t < 0 || t > 2)
+		{
+			rb_raise(rb_eArgError, "Invalid task");
+		}
+		config->task = NUM2INT(task);
+		break;
+	default:
+		rb_raise(rb_eArgError, "Invalid task");
+	}
+	return task;
+}
+
+static VALUE
 get_additional_equalities(EdlibAlignConfig *config)
 {
 	VALUE equalities = rb_ary_new();
@@ -119,8 +250,8 @@ get_additional_equalities(EdlibAlignConfig *config)
 	{
 		EdlibEqualityPair pair = config->additionalEqualities[i];
 		VALUE pair_ary = rb_ary_new();
-		rb_ary_push(pair_ary, INT2NUM(pair.first));
-		rb_ary_push(pair_ary, INT2NUM(pair.second));
+		rb_ary_push(pair_ary, rb_str_new(&pair.first, 1));
+		rb_ary_push(pair_ary, rb_str_new(&pair.second, 1));
 		rb_ary_push(equalities, pair_ary);
 	}
 
@@ -132,6 +263,48 @@ aligner_get_additional_equalities(VALUE self)
 {
 	EdlibAlignConfig *config = get_config(self);
 	return get_additional_equalities(config);
+}
+
+static VALUE
+set_additional_qualities(EdlibAlignConfig *config, VALUE equalities)
+{
+	Check_Type(equalities, T_ARRAY);
+	EdlibEqualityPair *pairs = (EdlibEqualityPair *)malloc(sizeof(EdlibEqualityPair) * RARRAY_LEN(equalities));
+
+	for (int i = 0; i < RARRAY_LEN(equalities); i++)
+	{
+		VALUE pair = rb_ary_entry(equalities, i);
+		Check_Type(pair, T_ARRAY);
+		if (RARRAY_LEN(pair) != 2)
+		{
+			rb_raise(rb_eArgError, "Invalid equality pair");
+		}
+		VALUE s1 = rb_ary_entry(pair, 0);
+		VALUE s2 = rb_ary_entry(pair, 1);
+		Check_Type(s1, T_STRING);
+		Check_Type(s2, T_STRING);
+		if (RSTRING_LEN(s1) != 1 || RSTRING_LEN(s2) != 1)
+		{
+			rb_raise(rb_eArgError, "String length must be 1");
+		}
+		char c1 = RSTRING_PTR(s1)[0];
+		char c2 = RSTRING_PTR(s2)[0];
+
+		pairs[i].first = c1;
+		pairs[i].second = c2;
+	}
+
+	config->additionalEqualities = pairs;
+	config->additionalEqualitiesLength = RARRAY_LEN(equalities);
+
+	return equalities;
+}
+
+static VALUE
+aligner_set_additional_equalities(VALUE self, VALUE equalities)
+{
+	EdlibAlignConfig *config = get_config(self);
+	return set_additional_qualities(config, equalities);
 }
 
 static VALUE
@@ -199,9 +372,12 @@ void Init_edlib(void)
 	rb_define_alloc_func(cAligner, config_allocate);
 	rb_define_method(cAligner, "initialize", aligner_initialize, 3);
 	rb_define_method(cAligner, "k", aligner_get_k, 0);
+	rb_define_method(cAligner, "k=", aligner_set_k, 1);
 	rb_define_method(cAligner, "mode", aligner_get_mode, 0);
+	rb_define_method(cAligner, "mode=", aligner_set_mode, 1);
 	rb_define_method(cAligner, "task", aligner_get_task, 0);
 	rb_define_method(cAligner, "additional_equalities", aligner_get_additional_equalities, 0);
+	rb_define_method(cAligner, "additional_equalities=", aligner_set_additional_equalities, 1);
 	rb_define_method(cAligner, "config", aligner_config_hash, 0);
 	rb_define_method(cAligner, "align", aligner_align, 2);
 }
